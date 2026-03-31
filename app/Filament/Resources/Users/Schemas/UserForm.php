60356d1e95a4a98808cb13ac6c186f139f8ab30d
louwinzky <louie.aringay@gmail.com>
2026-03-31 19:59:21 +0800

    remove two factor secret fields in admin

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -7,7 +7,6 @@ use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Section;
@@ -39,11 +38,6 @@ class UserForm
                 TextInput::make('password')
                     ->password()
                     ->required(),
-                Textarea::make('two_factor_secret')
-                    ->columnSpanFull(),
-                Textarea::make('two_factor_recovery_codes')
-                    ->columnSpanFull(),
-                DateTimePicker::make('two_factor_confirmed_at'),
                 Select::make('school_id')
                     ->relationship('school', 'name'),
 
